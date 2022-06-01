dae8e18a7c5252a52c51adfcc643a2dc0b0e4a5f
ScaleBurn <106637565+ScaleBurn@users.noreply.github.com>
2022-06-01 11:48:07 +1000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-let mySprite = sprites.create(img`
+let mySprite: Sprite = null
+game.splash("Idk name", "By G-Dev (Gabe,Dev,Advit)")
+let little_boi = sprites.create(img`
     . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . 
@@ -16,3 +18,80 @@ let mySprite = sprites.create(img`
     . . . . 7 7 . . 7 7 . . . . 
     . . . . f f . . f f . . . . 
     `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level1`)
+controller.moveSprite(little_boi)
+scene.cameraFollowSprite(little_boi)
+animation.runImageAnimation(
+mySprite,
+[img`
+    . . f f f . . . . . . . . f f f 
+    . f f c c . . . . . . f c b b c 
+    f f c c . . . . . . f c b b c . 
+    f c f c . . . . . . f b c c c . 
+    f f f c c . c c . f c b b c c . 
+    f f c 3 c c 3 c c f b c b b c . 
+    f f b 3 b c 3 b c f b c c b c . 
+    . c b b b b b b c b b c c c . . 
+    . c 1 b b b 1 b b c c c c . . . 
+    c b b b b b b b b b c c . . . . 
+    c b c b b b c b b b b f . . . . 
+    f b 1 f f f 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `,img`
+    . . f f f . . . . . . . . . . . 
+    f f f c c . . . . . . . . f f f 
+    f f c c . . c c . . . f c b b c 
+    f f c 3 c c 3 c c f f b b b c . 
+    f f b 3 b c 3 b c f b b c c c . 
+    . c b b b b b b c f b c b c c . 
+    . c b b b b b b c b b c b b c . 
+    c b 1 b b b 1 b b b c c c b c . 
+    c b b b b b b b b c c c c c . . 
+    f b c b b b c b b b b f c . . . 
+    f b 1 f f f 1 b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . c c . . c c . . . . . . . . 
+    . . c 3 c c 3 c c c . . . . . . 
+    . c b 3 b c 3 b c c c . . . . . 
+    . c b b b b b b b b f f . . . . 
+    c c b b b b b b b b f f . . . . 
+    c b 1 b b b 1 b b c f f f . . . 
+    c b b b b b b b b f f f f . . . 
+    f b c b b b c b c c b b b . . . 
+    f b 1 f f f 1 b f c c c c . . . 
+    . f b b b b b b f b b c c . . . 
+    c c f b b b b b c c b b c . . . 
+    c c c f f f f f f c c b b c . . 
+    . c c c . . . . . . c c c c c . 
+    . . c c c . . . . . . . c c c c 
+    . . . . . . . . . . . . . . . . 
+    `,img`
+    . . f f f . . . . . . . . . . . 
+    f f f c c . . . . . . . . f f f 
+    f f c c . . c c . . . f c b b c 
+    f f c 3 c c 3 c c f f b b b c . 
+    f f b 3 b c 3 b c f b b c c c . 
+    . c b b b b b b c f b c b c c . 
+    . c b b b b b b c b b c b b c . 
+    c b 1 b b b 1 b b b c c c b c . 
+    c b b b b b b b b c c c c c . . 
+    f b c b b b c b b b b f c . . . 
+    f b 1 f f f 1 b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `],
+200,
+false
+)
